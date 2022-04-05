b8ad4dc6229a8ffab599af2cd863c86ff54eeb6e
Swapnil <swapnilvernekar04@gmail.com>
2022-04-05 12:35:47 +0530

    Initial commit

--- ecommerce-front/src/core/Checkbox.js
+++ ecommerce-front/src/core/Checkbox.js
@@ -1,10 +1,32 @@
 import React,{useState, useEffect} from "react";
 
 
-const Checkbox = ({categories}) => {
+const Checkbox = ({categories, handleFilters}) => {
+     const[checked, setChecked ] = useState([]);
+
+     const handleToggle = c => () => {
+        const currentCategoryId = checked.indexOf(c); //return first index or -1
+        const newCheckedCategoryId = [...checked];
+        //if curently checked was not already in the state then we want to push 
+        //or pull or takeoff
+        if(currentCategoryId === -1)
+        {
+            newCheckedCategoryId.push(c);
+        }
+        else{
+            newCheckedCategoryId.splice(currentCategoryId,1)
+        }
+        //console.log(newCheckedCategoryId);
+        setChecked(newCheckedCategoryId);
+        handleFilters(newCheckedCategoryId); 
+
+
+     }
     return categories.map((c,i)=>(
         <li key={i} className="list-unstyled">
-            <input type="checkbox" className="form-check-input"/>
+            <input onChange={handleToggle(c._id)} 
+            value = {checked.indexOf(c._id === -1)}type="checkbox" 
+            className="form-check-input"/>
             <label className="form-check-label">{c.name}</label>
         </li>
     ));
--- ecommerce-front/src/core/apiCore.js
+++ ecommerce-front/src/core/apiCore.js
@@ -23,4 +23,27 @@ export const getCategories = () =>{
          
     })
     .catch(err => console.log(err));
-}
+};
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+    const data ={
+        limit,skip,filters
+    }
+    // console.log(name,email,password);
+    return fetch(`${API}/products/by/search`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            
+        },
+        body: JSON.stringify(data)
+
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
